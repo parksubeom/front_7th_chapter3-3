b171b6baabfb55b3be38d76b4684b609b89dd1be
Bumpist <104641096+parksubeom@users.noreply.github.com>
2025-12-12 01:45:46 +0900

    빌드 ... 왜이러니 증말

--- src/app/routers/App.tsx
+++ src/app/routers/App.tsx
@@ -4,24 +4,28 @@ import Footer from "@/widgets/Footer/ui/index.tsx"
 import PostsManagerPage from "@/pages/posts-manager/ui/Page.tsx"
 import { DashboardPage } from "@/pages/dashboard/ui/DashboardPage.tsx"
 import { SettingsPage } from "@/pages/settings/ui/SettingsPage.tsx"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 const App = () => {
+  const BASE_PATH = "/front_lite_chapter3-3/"
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router basename={BASE_PATH}>
       <div className="flex flex-col min-h-screen">
         <Header />
 
         <main className="flex-grow container mx-auto px-4 py-8">
           {/* Routes로 감싸서 URL에 따라 다른 컴포넌트를 보여줍니다 */}
           <Routes>
-            {/* 1. 메인 접속 시 대시보드로 자동 이동 */}
-            <Route path="/" element={<PostsManagerPage />} />
+            {/* 1. 메인 접속 시 게시글 관리페이지 자동 이동 */}
+            <Route path="/" element={<Navigate to="/posts" replace />} />
 
             {/* 2. 대시보드 페이지 */}
             <Route path="/dashboard" element={<DashboardPage />} />
 
-            {/* 3. 설정 페이지 */}
+            {/* 3. 게시글 관리 페이지 */}
+            <Route path="/posts" element={<PostsManagerPage />} />
+
+            {/* 4. 설정 페이지 */}
             <Route path="/settings" element={<SettingsPage />} />
           </Routes>
         </main>
